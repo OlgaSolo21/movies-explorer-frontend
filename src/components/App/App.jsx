05448866165eb16a87a26c5fe49a1b72034f4a76
Olga Solodovnikova <ossolodovnikova@yandex.ru>
2024-02-16 23:47:09 +0300

    feat: создание компонентов + верстка 2/20 компонента

---
+++ src/components/App/App.jsx
@@ -0,0 +1,13 @@
+
+import AboutTechs from "../Main/AboutTechs/AboutTechs";
+import AboutProject from "../Main/AboutProject/AboutProject";
+function App() {
+  return (
+      <>
+        <AboutProject/>
+        <AboutTechs/>
+      </>
+  )
+}
+
+export default App;
