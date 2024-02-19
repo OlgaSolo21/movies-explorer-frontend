f718da3cbfcb34327cecda7969f93a6d86393b8c
Olga Solodovnikova <ossolodovnikova@yandex.ru>
2024-02-19 11:54:41 +0300

    feat: aboutMe в процессе верстки

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,6 +2,7 @@ import PromoProject from "../Main/PromoProject/PromoProject";
 import {Route, Routes} from "react-router-dom";
 import AboutProject from "../Main/AboutProject/AboutProject";
 import AboutTechs from "../Main/AboutTechs/AboutTechs";
+import AboutMe from "../Main/AboutMe/AboutMe";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
               <Route path="/" element={<PromoProject/>}>
                   <Route path="aboutProject" element={<AboutProject/>} />
                   <Route path="aboutTechs" element={<AboutTechs/>} />
+                  <Route path="aboutMe" element={<AboutMe/>} />
               </Route>
           </Routes>
       </>
--- src/components/Main/AboutMe/AboutMe.jsx
+++ src/components/Main/AboutMe/AboutMe.jsx
@@ -1,3 +1,24 @@
+import profileImg from "../../../images/photo-1444703686981-a3abbc4d4fe3.avif"
 function AboutMe() {
-
+    return (
+        <section className="aboutMe">
+            <div className="text__head lineBottom">Студент</div>
+            <div className="aboutMe__info">
+                <h2 className="aboutMe__title">Olga</h2>
+                <p className="aboutMe__subtitle">Фронтенд-разработчик, 30 лет</p>
+                <p className="aboutMe__caption">
+                    Тут будет инфа обо мне Тут будет инфа обо мне
+                    'caption' cannot appear as a child of 'div'
+                    'caption' cannot appear as a child of 'div'
+                    'caption' cannot appear as a child of 'div'
+                    'caption' cannot appear as a child of 'div'
+                </p>
+                <img className="aboutMe__pic" src={profileImg} alt="фото профиля"/>
+                <a className="aboutMe__link" href="https://github.com/OlgaSolo21">GitHub</a>
+            </div>
+            {/*<Portfolio/>*/}
+        </section>
+    )
 }
+
+export default AboutMe
